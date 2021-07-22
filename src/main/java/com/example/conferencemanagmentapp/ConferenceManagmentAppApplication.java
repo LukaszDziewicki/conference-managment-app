c16ebcf32683ff20d7f7087ef6d3adc3161ff2b9
Lukasz <dziewickilukasz@gmail.com>
2021-07-22 12:53:09 +0200

    init project

---
+++ src/main/java/com/example/conferencemanagmentapp/ConferenceManagmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.conferencemanagmentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceManagmentAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceManagmentAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/conferencemanagmentapp/ConferenceManagmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conferencemanagmentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceManagmentAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
